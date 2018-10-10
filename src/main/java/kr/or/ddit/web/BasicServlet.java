c7945545b75da5b21b0fdd3ceb3ff63bb74dadce
iks <whiterainr@naver.com>
2018-10-10 17:49:44 +0900

    second commit

--- src/main/java/kr/or/ddit/web/BasicServlet.java
+++ src/main/java/kr/or/ddit/web/BasicServlet.java
@@ -8,7 +8,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-// 이클립스 git second commit
+// 이클립스 git second commits
 
 @WebServlet("/BasicServlet")
 public class BasicServlet extends HttpServlet {
